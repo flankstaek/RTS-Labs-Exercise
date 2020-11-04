f853405ae2800cf1013ef607b76a654a6379f93f
Ian Swain <conanreh@gmail.com>
2020-11-04 14:30:35 -0500

    Changed to HTTPS

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const searchTerm = createAsyncThunk(
   'search',
   async (query, thunkAPI) => {
-    const response = await axios.get('http://hn.algolia.com/api/v1/search?query=' + query)
+    const response = await axios.get('https://hn.algolia.com/api/v1/search?query=' + query)
     return response.data.hits;
   }
 )
